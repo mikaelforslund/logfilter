3550342ce4e53e76dde5affef236e58b842f1de1
Mikael Forslund <756148+mikaelforslund@users.noreply.github.com>
2021-04-28 17:40:52 +0800

    Revert "Revert "feat: Cleaned up the tokenizer to make it work, main function can take an expression in and echo lines from stdin that matches the expression to stdout""
    This reverts commit 30604a2614bb9248bf1d7325ac19e8e47cff4efe.

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,47 @@ mod tokenizer;
 mod grammar;
 
 use tokenizer::{ create_token, full_lines, Token };
-use std::io::{ self };
+use grammar:: { parse_expression };
+use std::io::{ self, Write };
+use log::{info};
+use simple_logger::SimpleLogger;
+use log::LevelFilter;
+use std::{env, process};
 
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
 fn main() -> Result<(), io::Error> {
-    // read lines from stdin, tokenises the words using regexps and finally writes same line to stdout... 
+    SimpleLogger::new().with_level(LevelFilter::Trace).init().unwrap();
+
+    /////////////////// TODO move this to clap... this is just for testing... 
+    let args: Vec<String> = env::args().collect();   
+
+    if args.len() == 1 {
+        info!("\n\nUsage: semfilter [expr] < FILE\n");
+        process::exit(-1);
+    }
+    info!("arg: {:?}", args[1]);
+    ////////////////////
+
+    // read lines from stdin, tokenizes the words using regexps and finally writes same line to stdout if it 
+    // matches the expression passed in to the program 
     for line in full_lines(io::stdin().lock()) {
         let line = line?;
 
-        for token in line.split_whitespace() {
-            let token:Token = create_token(&token.to_string());
-            println!("{:?}", token);
-        }
+        let tokens:Vec<Token> = line.split_whitespace()
+            .into_iter()
+            .map(|word| create_token(&word))
+            .collect();
 
-        println!("full line: {}", line);
+        info!("main.tokens: {:?}", tokens);
+
+        if parse_expression(&args[1], &tokens) {
+            let stdout = io::stdout();
+            let mut handle = stdout.lock();
+            handle.write_all(line.as_bytes())?;
+        }
     }
 
     Ok(())
